a7d8755ddf64ec66da8ad7c370ed07d7e2528bad
jbt <hilaterem2008@gmail.com>
2026-09-08 14:23:13 +0300

    changes

--- main.py
+++ main.py
@@ -34,22 +34,18 @@ def check_touch_mines(legs, mines):
 
 def touch_mine():
     return
-# def main():
-#     pygame.init()
-#     while True:
-#         screen.draw_bush(consts.bush_img)
-#         screen.starting_location(consts.img_solider,consts.flag_img)
-#         for event in pygame.event.get():
-#             if event.type==pygame.quit():
-#                 pygame.quit()
-#                 exit()
-#         #draw all our elemnts
-#         #update everything
-#
-#
-#
-# if __name__ == '__main__':
-#     main()
+def main():
+    pygame.init()
+    while state["is_window_open"]:
+        screen.draw_bush(consts.bush_img)
+        screen.starting_location(consts.img_solider,consts.flag_img)
+        #draw all our elemnts
+        #update everything
+
+
+
+if __name__ == '__main__':
+    main()
 
 def handle_user_events():
     for event in pygame.event.get():
