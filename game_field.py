b8c23f51b75584a6cf42bed144fde2fdcb9bb190
jbt <hilaterem2008@gmail.com>
2026-09-08 09:57:31 +0300

    images

--- game_field.py
+++ game_field.py
@@ -2,5 +2,8 @@ import consts
 board = []
 def make_board():
     for x in range(consts.BOARD_ROWS):
+        row = []
         for y in range(consts.BOARD_COLS):
             board[x][y] = consts.E
+
+print(make_board())
