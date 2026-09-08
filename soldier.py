6b761d017ac95c37d8fd8dfd83ea659ac44055f4
jbt <hilaterem2008@gmail.com>
2026-09-08 14:16:28 +0300

    changes

--- soldier.py
+++ soldier.py
@@ -1,17 +1,15 @@
 import consts
 
 player = {
-    "Player": {
         "position_x": 0,
         "position_y": 0,
         "health": 3
-    }
 }
 
 def create_solider_body():
     list_of_solider_body_places = []
-    x_solider = consts.SOLDIER_BODY_ROWS
-    y_solider = consts.SOLDIER_COLS
+    x_solider = player["position_x"]
+    y_solider = player["position_y"]
     for x in range(x_solider, x_solider + consts.SOLDIER_BODY_ROWS):
         for y in range(y_solider, y_solider + consts.SOLDIER_COLS):
             tup = (x, y)
@@ -20,8 +18,8 @@ def create_solider_body():
 
 def create_solider_legs():
     list_of_solider_leg_places = []
-    x_solider = consts.SOLDIER_FEET_ROWS
-    y_solider = consts.SOLDIER_COLS
+    x_solider = player["position_x"]
+    y_solider = player["position_y"] + consts.SOLDIER_BODY_ROWS
     for x in range(x_solider, x_solider + consts.SOLDIER_FEET_ROWS):
         for y in range(y_solider, y_solider + consts.SOLDIER_COLS):
             tup = (x, y)
