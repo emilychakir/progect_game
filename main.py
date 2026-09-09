d2a09255fc854867bc7deab17d774282f909e2d7
jbt <aya786321@gmail.com>
2026-09-09 09:30:05 +0300

    HJ

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import screen
 import consts
 import game_field
 import soldier
+from soldier import player
+
 Screen = screen.screen
 state = {
     "is_window_open": True,
@@ -25,6 +27,7 @@ def handle_user_events():
             elif event.key == pygame.K_DOWN:
                 if soldier.player["position_y"] < consts.BOARD_COLS - 1:
                     soldier.player["position_y"] += 1
+
 
 
 
@@ -56,6 +59,9 @@ def check_touch_mines(legs, mines):
         if leg_place in mines:
             return True
     return False
+
+# def check_if_location_legal():
+#     if soldier.player["position_x"]+1>consts.BOARD_COLS:
 
 
 def main():
--- screen.py
+++ screen.py
@@ -2,6 +2,7 @@ import  pygame
 import random
 import consts
 import game_field
+from game_field import mines_places
 
 screen = pygame.display.set_mode(
         (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
@@ -35,14 +36,33 @@ def flag_location():
 
 def location_of_soldier(location):
     screen.blit(consts.soldier, (location[0], location[1]))
+
 def draw_matrix(board1):
+    screen.fill((0, 0, 0))
     for r in range(consts.BOARD_ROWS):
         for c in range(consts.BOARD_COLS):
-            pygame.draw.rect(board1,(144, 238, 144),(c*consts.CELL_SIZE,r*consts.CELL_SIZE,consts.CELL_SIZE,consts.CELL_SIZE),1)
+            pygame.draw.rect(screen,
+                             (144, 238, 144),
+                             (c*consts.CELL_SIZE,
+                              r*consts.CELL_SIZE,
+                              consts.CELL_SIZE,
+                              consts.CELL_SIZE),
+                             1)
+    for mine in range(len(game_field.mines_places)):
+        mine=game_field.mines_places[mine]
+        pygame.draw.rect(screen,
+                         (136, 8, 8),
+                         (mine[0] * consts.CELL_SIZE,
+                          mine[1] * consts.CELL_SIZE,
+                          consts.CELL_SIZE,
+                          consts.CELL_SIZE),
+                         )
+
+
 
 def draw_game(tuple_location):
     draw_background()
-    draw_matrix(game_field.board1)
+
     draw_bush(location)
     flag_location()
     location_of_soldier(tuple_location)
